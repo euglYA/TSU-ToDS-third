6fcd47340f842ec9948e44c40e5235f2571f0f1d
euglYA <90235668+euglYA@users.noreply.github.com>
2023-08-23 21:41:33 +0700

    funcs for button in ChartsComponent
    all buttons in ChartsComponent have their own usages

--- tods_3/chartscomponent.cpp
+++ tods_3/chartscomponent.cpp
@@ -34,11 +34,49 @@ ChartsComponent::ChartsComponent(QWidget *parent)
     this->vLayout->addLayout(this->hLayout);
     this->vLayout->addWidget(this->chartView);
 
+    QObject::connect(this->chooseChartType, &QComboBox::currentTextChanged, this, &ChartsComponent::changeChartType);
+    QObject::connect(this->swapChartTheme, &QCheckBox::stateChanged, this, &ChartsComponent::swapBlackAndWhiteTheme);
+    QObject::connect(this->saveAsPDF, &QPushButton::clicked, this, &ChartsComponent::saveChartAsPdf);
+}
+
+void ChartsComponent::changeChartType(const QString &text) {
+    if (text == this->chartType->getType())
+        return;
+
+    if (text == "BarChart")
+        this->chartType = new BarChart;
+    else if (text == "PieChart")
+        this->chartType = new PieChart;
+    else
+        return;
+
+    this->rerenderChart();
 }
 
 void ChartsComponent::changeChartData(QMap<QString, QVariant> data) {
     this->_data = data;
     this->rerenderChart();
+}
+
+void ChartsComponent::swapBlackAndWhiteTheme(bool state) {
+    if (state)
+        this->_chart->setTheme(QChart::ChartThemeHighContrast);
+    else
+        this->_chart->setTheme(QChart::ChartThemeQt);
+}
+
+void ChartsComponent::saveChartAsPdf() {
+    if (this->_data.size() == 0)
+        return;
+
+    QString dir = QFileDialog::getSaveFileName(this, "Save chart as PDF", QDir::homePath(), "PDF files (*.pdf)");
+
+    QPdfWriter writer(dir);
+    writer.setCreator("euglYA");
+    writer.setPageSize(QPageSize::A4);
+    QPainter painter(&writer);
+    this->chartView->render(&painter);
+    painter.end();
 }
 
 void ChartsComponent::rerenderChart() {
